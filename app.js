9771bea0bab7102ba42307ca15d16986a02fdfbc
Brandon Jones <brandon@paperplanes.digital>
2018-05-02 11:15:47 -0400

    updated app.js from lorelei.js

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-//thought: leave price list on backpage; (maybe) ask client to leave name&phone;
-//[delete]cat mario[delete]
 var restify = require('restify');
 var builder = require('botbuilder');
 var apiai = require('./apiai_recognizer');
-var utils = require('./utils');
 
 //create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
@@ -17,8 +14,27 @@ var inMemoryStorage = new builder.MemoryBotStorage();
 //receive messages from the user and respond
 const bot = new builder.UniversalBot(connector,{
 	// persistConversationData: true
-})
+})    
 	.set('storage', inMemoryStorage);;
+
+//create custom intent reconizer
+bot.recognizer({
+	recognize: function (context, done) {
+	var intent = { score: 0.0 };
+	
+			if (context.message.text) {
+				switch (context.message.text.toLowerCase()) {
+					case 'help':
+						intent = { score: 1.0, intent: 'Help' };
+						break;
+					case 'goodbye':
+						intent = { score: 1.0, intent: 'Goodbye' };
+						break;
+				}
+			}
+			done(null, intent);
+		}
+	});	
 
 //setup restify server
 var server = restify.createServer();
@@ -29,50 +45,229 @@ server.listen(process.env.port || process.env.PORT || 3978, function(){
 //listen for message from users
 server.post('/api/messages', connector.listen());
 
-//Main Dialog
-// bot.recognizer(apiai.recognizer);
-//Begin Dialog Opener
-bot.dialog('/', [
-	function (session, args, next){
-		session.send('[Start Root Dialog]');
-		session.userData.profile = session.userData.profile || initialProfile;
-		session.beginDialog('opener:/');
+intents.matches('Intent.Greeting','/intent.greeting');
+intents.matches('Intent.Flattery', '/intent.flattery');
+intents.matches('Intent.Get_Pic','/intent.get_pic');
+intents.matches('Intent.Service_Inquiry','/intent.service_inquiry');
+intents.matches('Intent.Price_Inquiry','/intent.price_inquiry');
+intents.matches('Intent.Availability', '/intent.availability');
+intents.matches('Intent.Transportation_Inquiry','/intent.transportation_inquiry');
+intents.matches('Intent.Location_Inquiry','/intent.location_inquiry');
+intents.matches('Intent.Give_Location','/intent.give_location');
+intents.matches('Intent.Give_TimeSlot','/intent.give_timeslot');
+
+bot.dialog('/', intents);
+
+var askName = require('./askName.js');
+bot.dialog('askName', askName.askName);
+
+var saveDialogData = function (session, args, next){
+	session.userData.profile = args.response;
+	session.endDialog();
+};
+
+//intent_handler: /intent.greeting
+bot.dialog('/intent.greeting', [
+	function(session, args, next){
+		session.userData.profile = session.userData.profile || {};
+		var entities = args.entities;
+		if(!session.userData.profile.name){
+			session.userData.profile.name = getName(entities);
+		}
+		var name = session.userData.profile.name || 'sweetie';
+		var reply = `Hey there, ${name}.`;
+		if(!session.userData.profile.name){
+			var data = {profile: session.userData.profile, reply: reply};
+			session.beginDialog('askName', data);
+		}
+		else{
+			session.endDialog(reply);
+		}
 	},
-	function (session, args, next){
-		// session.beginDialog('main:/');
-		session.send('[Start Main Dialog]');
-	}
+	saveDialogData
 ]);
 
-bot.library(require('./opener').createLibrary());
-bot.library(require('./askName').createLibrary());
-bot.library(require('./confirmTime').createLibrary());
-
-const initialProfile = {
-	default: {
-		model: 'Gina',
-		neighborhood: 'Rome'
-	},
-	appointment: {
-		'exact-time': [],
-		'relative-time': [],
-		service: [],
-		price: [],
-		location: [],	
-		model: ''
-	},
-	demographic: {
-		name: ''
-	},
-	confirmation: {
-		time: {
-			hour: null, minute: null, date: null
-		},
-		location: {
-			neighborhood: '', site: '', address: ''
-		},
-		service: {
-			'service-in-out': '', duration: '', addon: ''
+//intent_handler: /intent.flattery
+bot.dialog('/intent.flattery', [
+	function(session, args, next){
+		session.userData.profile = session.userData.profile || {};
+		var entities = args.entities;
+		if(!session.userData.profile.name){
+			session.userData.profile.name = getName(entities);
 		}
+		var name = session.userData.profile.name || '';
+		var reply = `I'm not saying flattery won't get you anywhere, but I'm not saying it will.`;
+		if(!session.userData.profile.name){
+			var data = {profile: session.userData.profile, reply: reply};
+			session.beginDialog('askName', data);
+		}
+		else{
+			session.endDialog(reply);
+		}
+	},
+	saveDialogData	
+]);
+
+//intent_handler: /intent.get_pic
+bot.dialog('/intent.get_pic', [
+	function(session, args, next){
+		session.userData.profile = session.userData.profile || {};
+		var entities = args.entities;
+		if(!session.userData.profile.name){
+			session.userData.profile.name = getName(entities);
+		}
+		var name = session.userData.profile.name || '';
+		var reply = `I'll show you mine if you show me yours.`;
+		if(!session.userData.profile.name){
+			var data = {profile: session.userData.profile, reply: reply};
+			session.beginDialog('askName', data);
+		}
+		else{
+			session.endDialog(reply);
+		}
+	},
+	saveDialogData		
+]);
+
+//intent_handler: /intent.service_inquiry
+
+
+//intent_handler: /intent.price_inquiry
+bot.dialog('/intent.price_inquiry', [
+	function(session, args, next){
+		session.userData.profile = session.userData.profile || {};
+		var entities = args.entities;
+		if(!session.userData.profile.name){
+			session.userData.profile.name = getName(entities);
+		}
+		var name = session.userData.profile.name || '';
+		var reply = `it's $50.`;
+		if(!session.userData.profile.name){
+			var data = {profile: session.userData.profile, reply: reply};
+			session.beginDialog('askName', data);
+		}
+		else{
+			session.endDialog(reply);
+		}
+	},
+	saveDialogData		
+]);
+
+//intent_handler: /intent.availability
+bot.dialog('/intent.availability', [
+	function(session, args, next){
+		session.userData.profile = session.userData.profile || {};
+		var entities = args.entities;
+		if(!session.userData.profile.name){
+			session.userData.profile.name = getName(entities);
+		}
+		var name = session.userData.profile.name || '';
+		if(name){
+			name += ' ';
+		}
+		var reply = `Sry ${name}I'm pretty busy right now, but let's set something up later.`;
+		if(!session.userData.profile.name){
+			var data = {profile: session.userData.profile, reply: reply};
+			session.beginDialog('askName', data);
+		}
+		else{
+			session.endDialog(reply);
+		}
+	},
+	saveDialogData		
+]);
+
+//intent_handler: /intent.availability
+bot.dialog('/intent.give_timeslot', [
+	function(session, args, next){
+		session.userData.profile = session.userData.profile || {};
+		var entities = args.entities;
+		if(!session.userData.profile.name){
+			session.userData.profile.name = getName(entities);
+		}
+		var name = session.userData.profile.name || '';
+		var reply = `I'm pretty booked up then.  Mayb later.`;
+		if(!session.userData.profile.name){
+			var data = {profile: session.userData.profile, reply: reply};
+			session.beginDialog('askName', data);
+		}
+		else{
+			session.endDialog(reply);
+		}
+	},
+	saveDialogData		
+]);
+
+//intent_handler: /intent.transportation
+bot.dialog('/intent.transportation_inquiry', [
+	function(session, args, next){
+		session.userData.profile = session.userData.profile || {};
+		var entities = args.entities;
+		if(!session.userData.profile.name){
+			session.userData.profile.name = getName(entities);
+		}
+		var name = session.userData.profile.name || '';
+		var reply = `send your address. i'll uber.`;
+		if(!session.userData.profile.name){
+			var data = {profile: session.userData.profile, reply: reply};
+			session.beginDialog('askName', data);
+		}
+		else{
+			session.endDialog(reply);
+		}
+	},
+	saveDialogData		
+]);
+
+//intent_handler: /intent.give_location
+bot.dialog('/intent.give_location', [
+	function(session, args, next){
+		session.userData.profile = session.userData.profile || {};
+		var entities = args.entities;
+		if(!session.userData.profile.name){
+			session.userData.profile.name = getName(entities);
+		}
+		var name = session.userData.profile.name || '';
+		var reply = `send your address. i'll uber.`;
+		if(!session.userData.profile.name){
+			var data = {profile: session.userData.profile, reply: reply};
+			session.beginDialog('askName', data);
+		}
+		else{
+			session.endDialog(reply);
+		}
+	},
+	saveDialogData			
+]);
+
+//intent_handler: /intent.location_inquiry
+bot.dialog('/intent.location_inquiry', [
+	function(session, args, next){
+		session.userData.profile = session.userData.profile || {};
+		var entities = args.entities;
+		if(!session.userData.profile.name){
+			session.userData.profile.name = getName(entities);
+		}
+		var name = session.userData.profile.name || '';
+		var reply = `i got a place in buckhead tonight.`;
+		if(!session.userData.profile.name){
+			var data = {profile: session.userData.profile, reply: reply};
+			session.beginDialog('askName', data);
+		}
+		else{
+			session.endDialog(reply);
+		}
+	},
+	saveDialogData		
+]);
+
+
+function getName(entities){
+	if (entities.hasOwnProperty('entity-name')){
+		return entities['entity-name'];
+	}	
+	else{
+		return '';
 	}
-};
+}
+
